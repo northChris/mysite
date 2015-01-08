543fee0439bb1dfc3069539073a6f1b325862759
vagrant <vagrant@precise32.(none)>
2015-01-08 23:27:29 +0000

    init commit of mysite

---
+++ polls/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from polls.models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+admin.site.register(Question, QuestionAdmin)
